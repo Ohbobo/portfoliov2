60863db6a05dfa85e6992607657f438f0edb331b
theo <agentheo0@gmail.com>
2023-10-22 20:56:50 +0200

    refacto contact

---
+++ frontend/src/components/react/contact/data/ApiService.ts
@@ -0,0 +1,30 @@
+interface ContactFormData {
+    name: string;
+    email: string;
+    phone: string;
+    message: string;
+}
+
+export class ContactApiService {
+    async sendMail(formData: ContactFormData) {
+        try {
+            const response = await fetch(`http://localhost:3000/mail/send?email=${formData.email}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+            });
+
+            if(!response.ok){
+                throw new Error(`La requête a échoué avec le statut ${response.status}`);
+            }
+    
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error:', error);
+            throw new Error(`Erreur lors de la récupération des données: ${error}`);
+        }
+    }
+}
--- frontend/src/components/react/contact/presentation/ContactForm.tsx
+++ frontend/src/components/react/contact/presentation/ContactForm.tsx
@@ -1,114 +1,146 @@
 import React from 'react'
-import { useState } from 'react'
-import { CardContact } from './cardContact';
+import { useState, useEffect } from 'react'
+import { CardContact } from './CardContact';
+import { ContactApiService } from '../data/ApiService';
 
 export const ContactForm = () => {
 
     const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        phone: "",
-        message: "",
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
     });
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [isSent, setIsSent] = useState(false);
+    const [showMessage, setShowMessage] = useState(false);
+
+    useEffect(() => {
+        let timer: NodeJS.Timer;
+        if (isSent) {
+            setShowMessage(true);
+            timer = setTimeout(() => {
+                setShowMessage(false);
+                setIsSent(false);
+            }, 5000);
+        }
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [isSent]);
 
     const handleChange = (e: { target: { id: any; value: any; }; }) => {
         setFormData({ ...formData, [e.target.id]: e.target.value });
     };
 
-    
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
+    const resetForm = () => {
+        setFormData({
+            name: '',
+            email: '',
+            phone: '',
+            message: '',
+          });
+    }
+ 
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        setIsLoading(true);
 
-    fetch(`http://localhost:3000/mail/send?email=${formData.email}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: formData.name,
-        email: formData.email,
-        phone: formData.phone,
-        message: formData.message,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error("Error:", error));
-  };
+        const contactApiService = new ContactApiService();
 
-  return (
-    <section className="bg-gray-100 rounded-lg">
-        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-            <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
-            <div className="lg:col-span-2 lg:py-12 flex flex-col justify-center">
-                <CardContact />
-            </div>
+        try {
+            const data = await contactApiService.sendMail(formData);
+            setIsSent(true);
+            setIsLoading(false);
+            resetForm();
+        } catch (error) {
+            console.error('Error:', error);
+            setIsLoading(false);
+        }
+    };
 
-            <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
-                <form onSubmit={handleSubmit} className="space-y-4">
-                <div>
-                    <label className="sr-only" htmlFor="name">Name</label>
-                    <input
-                    className="block w-full rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black peer-checked:border-black peer-checked:bg-black peer-checked:text-white"
-                    placeholder="Nom / prénom"
-                    type="text"
-                    id="name"
-                    name='name'
-                    onChange={handleChange}
-                    />
+    return (
+        <section className="bg-gray-100 rounded-lg">
+            <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+                <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
+                <div className="lg:col-span-2 lg:py-12 flex flex-col justify-center">
+                    <CardContact />
                 </div>
 
-                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+                {showMessage  && (
+                <div className="text-green-500 mb-4">Votre message a bien été envoyé !</div>
+                )}
+                    <form onSubmit={handleSubmit} className="space-y-4">
                     <div>
-                    <label className="sr-only" htmlFor="email">Email</label>
-                    <input
+                        <label className="sr-only" htmlFor="name">Name</label>
+                        <input
                         className="block w-full rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black peer-checked:border-black peer-checked:bg-black peer-checked:text-white"
-                        placeholder="Adresse email"
-                        type="email"
-                        id="email"
-                        name='email'
+                        placeholder="Nom / prénom"
+                        type="text"
+                        id="name"
+                        name='name'
                         onChange={handleChange}
-                    />
+                        value={formData.name}
+                        />
+                    </div>
+
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                        <div>
+                        <label className="sr-only" htmlFor="email">Email</label>
+                        <input
+                            className="block w-full rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black peer-checked:border-black peer-checked:bg-black peer-checked:text-white"
+                            placeholder="Adresse email"
+                            type="email"
+                            id="email"
+                            name='email'
+                            onChange={handleChange}
+                            value={formData.email}
+                        />
+                        </div>
+
+                        <div>
+                        <label className="sr-only" htmlFor="phone">Phone</label>
+                        <input
+                            className="block w-full rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black peer-checked:border-black peer-checked:bg-black peer-checked:text-white"
+                            placeholder="Téléphone"
+                            type="tel"
+                            id="phone"
+                            name='phone'
+                            value={formData.phone}
+                            onChange={handleChange}
+                        />
+                        </div>
                     </div>
 
                     <div>
-                    <label className="sr-only" htmlFor="phone">Phone</label>
-                    <input
+                        <label className="sr-only" htmlFor="message">Message</label>
+
+                        <textarea
                         className="block w-full rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black peer-checked:border-black peer-checked:bg-black peer-checked:text-white"
-                        placeholder="Téléphone"
-                        type="tel"
-                        id="phone"
-                        name='phone'
+                        placeholder="Message"
+                        rows={8}
+                        id="message"
+                        name='message'
+                        value={formData.message}
                         onChange={handleChange}
-                    />
+                        ></textarea>
                     </div>
-                </div>
 
-                <div>
-                    <label className="sr-only" htmlFor="message">Message</label>
-
-                    <textarea
-                    className="block w-full rounded-lg border border-gray-200 p-3 text-gray-600 hover:border-black peer-checked:border-black peer-checked:bg-black peer-checked:text-white"
-                    placeholder="Message"
-                    rows={8}
-                    id="message"
-                    name='message'
-                    onChange={handleChange}
-                    ></textarea>
+                    <div className="mt-4">
+                        <button
+                        type="submit"
+                        className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
+                        disabled={isLoading}
+                        >
+                        {isLoading ? "Envoi en cours..." : 'Envoyer'}
+                        </button>
+                    </div>
+                    </form>
                 </div>
-
-                <div className="mt-4">
-                    <button
-                    type="submit"
-                    className="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
-                    >
-                    Envoyer
-                    </button>
                 </div>
-                </form>
             </div>
-            </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
